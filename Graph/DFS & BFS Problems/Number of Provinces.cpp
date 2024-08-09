18a6c16e8eb8fd1ade768eea0b5306656d291f87
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-08-09 23:38:54 +0530

    Number of Provinces

--- Graph/DFS & BFS Problems/Number of Provinces.cpp
+++ Graph/DFS & BFS Problems/Number of Provinces.cpp
@@ -28,3 +28,45 @@ public:
         return count; 
     }
 };
+
+
+
+
+/// GFG Adjancency matrix into adjacency list
+
+    void dfs(vector<int>ad[], vector<bool>&visited, int src){
+        visited[src] = true;
+        
+        for(auto it: ad[src]){
+            if(visited[it] == false){
+                dfs(ad, visited, it);
+            }
+        }
+        
+    }
+
+
+    int numProvinces(vector<vector<int>> adj, int V) {
+        
+        vector<int>ad[V];
+        for(int i = 0; i < V; i++){
+            for(int j = 0; j < V; j++){
+                if(adj[i][j] == 1 && i != j){
+                    ad[i].push_back(j);
+                    ad[j].push_back(i);
+                }
+            }
+        }
+        
+        
+        // code here
+        vector<bool>visited(V, false);
+        int provinces = 0;
+        for(int i = 0; i < V; i++){
+            if(visited[i] == false){
+                provinces++;
+                dfs(ad, visited, i);
+            }
+        }
+        return provinces;
+    }
