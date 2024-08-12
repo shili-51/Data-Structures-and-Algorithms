4fbb266ec267d83053210e1e8a3cfc77db0e114a
Shilpy <79649004+shili-51@users.noreply.github.com>
2024-08-13 01:53:51 +0530

    Update Network Delay Time.cpp

--- Graph/Shotest Path Algo/Network Delay Time.cpp
+++ Graph/Shotest Path Algo/Network Delay Time.cpp
@@ -1,3 +1,4 @@
+// Priority queue solution but can also be solved with q data structure
 
    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
 
@@ -40,3 +41,51 @@
         return minTime;
 
     }
+
+
+
+// queue based solution
+
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+
+        vector<pair<int,int>>adj[n+1];
+
+        for(int i = 0; i < times.size(); i++){
+            int u = times[i][0];
+            int v = times[i][1];
+            int time = times[i][2];
+            adj[u].push_back({v, time});
+        }
+
+        vector<int>timeTaken(n+1, INT_MAX);
+        // {time , {node}}
+        queue<int>q;
+        timeTaken[k] = 0;
+        q.push(k);
+
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+
+            for(auto it: adj[node]){
+                int adjNode = it.first;
+                int adjNodeTime = it.second;
+
+                if(timeTaken[adjNode] > timeTaken[node] + adjNodeTime){
+                    timeTaken[adjNode] = timeTaken[node] + adjNodeTime;
+                    q.push(adjNode);
+                }
+            }
+        }
+        
+        int minTime = 0;
+        for(int i = 1; i <= n; i++){
+            if(timeTaken[i] == INT_MAX) return -1;
+            minTime = max(minTime, timeTaken[i]);
+        }
+        return minTime;
+
+    }
+};
